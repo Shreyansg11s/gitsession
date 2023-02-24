14c6779fce7138202b44d123247b6a8b0e859e3f
Shreyansg11s <124326059+Shreyansg11s@users.noreply.github.com>
2023-02-24 09:03:22 +0545

    Add files via upload

---
+++ stableSorting.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int foo [5];
+int i;
+int j;
+int A[i];
+int A[j];
+int m;
+int n;
+}
+
+solve(){
+
+i < j and A[i] = A[j] and m < n;
+
+for {
+
+if(i<j)
+{
+	
+}
+
+
+}
+
+}
